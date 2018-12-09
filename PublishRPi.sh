8f0d18ad8a7d16ecc5802a5abc972f6a0fecd949
Niels Penneman <niels@penneman.org>
2018-12-09 20:12:10 +0100

    Packaging for Raspberry Pi

---
+++ PublishRPi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Script to build a .deb package for deployment to a Raspberry Pi.
+#
+set -e
+CONFIGURATION='Release'
+FRAMEWORK='netcoreapp2.0'
+RUNTIME='linux-arm'
+ARGS="-c ${CONFIGURATION} -f ${FRAMEWORK} -r ${RUNTIME}"
+find "./bin/Release/${FRAMEWORK}/${RUNTIME}/" -name "ngswview.*.${RUNTIME}deb" -delete
+dotnet clean ${ARGS}
+dotnet build ${ARGS}
+dotnet deb ${ARGS}
+DEBFILE=`find "./bin/Release/${FRAMEWORK}/${RUNTIME}/" -name "ngswview.*.${RUNTIME}.deb"`
+rm -rf .rpi.deb
+mkdir .rpi.deb
+cd .rpi.deb
+ar p "../${DEBFILE}" control.tar.gz | tar -xz
+sed -i'' 's/\(Architecture: \)amd64/\1armhf/' control
+tar czf control.tar.gz *[!z]
+ar r "../${DEBFILE}" control.tar.gz
+cd -
+rm -rf .rpi.deb
+echo "Package file: ${DEBFILE}"
